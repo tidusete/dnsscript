89dcf58e3e4afa2e8ad04e608ddb9859520d5fba
tidusete <jordicidsierra@gmail.com>
2018-07-18 21:30:16 +0200

    Final
    Falta pulir el type Text

--- job.py
+++ job.py
@@ -4,7 +4,7 @@ import csv
 dominis = ["wowhead.com","wowchakra.com"]
 
 from subprocess import call
-
+header = ["dominio"]
 aux = []
 for domini in dominis:
     
@@ -23,20 +23,51 @@ for domini in dominis:
             cont= 0
 
             while (not trobat and cont < len(tipus)):
-
+                if (row[0] not in header):
+                    header.append(row[0])
+                 
                 if (row[0] == tipus[cont]):
                     trobat=True
-                    textAcumulat[cont]=textAcumulat[cont]+""+row[1]+" "+row[2]+"||"
+                    textAcumulat[cont]=textAcumulat[cont]+" + "+row[1]+" "+row[2]
                 cont=cont+1
             if (not trobat):
                 tipus.append(row[0])
-                textAcumulat.append(row[1]+" "+row[2]+"||")
+                textAcumulat.append(row[1]+" "+row[2])
                 
 
         #print(tipus)
         #print(textAcumulat)
         #print(domini)
         aux2 = [domini,tipus,textAcumulat]
+
+
         aux.append(aux2)
- 
-print(aux)
+trash=[]
+#abrimos csv final
+with open('dnsfinal.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=",")
+    spamwriter.writerow(header)
+    for domini,nomdomini in enumerate(dominis):
+        trash=[]
+
+        trash.append(aux[domini][0])
+
+        index=1
+        while (index < len(header)):
+            trobat3=False
+            cont=1
+
+            while (not trobat3 and cont < len(aux[domini][1])):
+                if(aux[domini][1][cont] == header[index]):
+                    trobat3=True
+                    trash.append(aux[domini][2][cont])
+                cont=cont+1
+            if (not trobat3):
+                trash.append(",")
+            index=index+1
+        spamwriter.writerow(trash)
+
+
+
+
+
