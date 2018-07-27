e5100b585f6a9d7302fc1bfe844634a326f10b7d
tidusete <jordicidsierra@gmail.com>
2018-07-27 08:23:58 +0200

    CSVfinal amb tots excel

--- job.py
+++ job.py
@@ -1,49 +1,51 @@
 import csv
 
 
-dominis = ["wowhead.com","wowchakra.com"]
+from openpyxl import load_workbook		#Lectura excel de dominios
+wb = load_workbook(filename='LISTADO.xlsx')
+#ws = wb['Poner nombre de la hoja']
+
+colDominios = ws['A']
+
+malDominis = list(colDominios)
+dominis=[]
+for x in range (5,len(malDominis)):
+    dominis.append(malDominis[x].value.encode("utf-8"))
+
 
 from subprocess import call
 header = ["dominio"]
 aux = []
 for domini in dominis:
-    
-
-    call(["python", "dnsrecon/dnsrecon.py", "-d", domini, "-t", "std", "-c", "csv_"+domini])
-    with open ("csv_"+domini,"rb") as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-
-        tipus = []
-        textAcumulat = []
-        #aux = [tipus, textAcumulat, domini]
-        
-        for row in spamreader:
-            #print(row)
-            trobat=False
-            cont= 0
-
-            while (not trobat and cont < len(tipus)):
-                if (row[0] not in header):
-                    header.append(row[0])
-                 
-                if (row[0] == tipus[cont]):
-                    trobat=True
-                    textAcumulat[cont]=textAcumulat[cont]+" + "+row[1]+" "+row[2]
-                cont=cont+1
-            if (not trobat):
-                tipus.append(row[0])
-                textAcumulat.append(row[1]+" "+row[2])
-                
-
-        #print(tipus)
-        #print(textAcumulat)
-        #print(domini)
-        aux2 = [domini,tipus,textAcumulat]
-
-
+    try:
+        call(["python", "dnsrecon/dnsrecon.py", "-d", domini, "-t", "std", "-c", "csv_"+domini])
+        with open ("csv_"+domini,"rb") as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',')
+            tipus = []
+            textAcumulat = []
+            for row in spamreader:
+                #print(row)
+                trobat=False
+                cont= 0
+                while (not trobat and cont < len(tipus)):
+                    if (row[0] not in header):
+                        header.append(row[0])
+                    if (row[0] == tipus[cont]):
+                        trobat=True
+                        textAcumulat[cont]=textAcumulat[cont]+" + "+row[1]+" "+row[2]
+                    cont=cont+1
+                if (not trobat):
+                    tipus.append(row[0])
+                    textAcumulat.append(row[1]+" "+row[2])
+            aux2 = [domini,tipus,textAcumulat]
+            aux.append(aux2)
+        call(["rm", "csv_"+domini])
+    except Exception:
+        aux2 = [domini,[],[]]
         aux.append(aux2)
-trash=[]
+
 #abrimos csv final
+trash=[]
 with open('dnsfinal.csv', 'wb') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=",")
     spamwriter.writerow(header)
